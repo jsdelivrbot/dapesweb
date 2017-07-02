2cdad1675a7a6c56c8ced12fbe9e11c815ded824
Roje Cursino <rojecursino@gmail.com>
2017-07-02 17:40:42 -0300

    correções do formulário de cadastro sobre você

---
+++ src/models/user.model.ts
@@ -0,0 +1,23 @@
+export class UserModel{
+    nome: string;
+    email: string;
+    cpf: string;
+    nacionalidade:string;
+    naturalidade:string;
+    sexo:string;
+    nascimento: Date;
+    estadoCivil:string;
+    conjuge:string;
+    telefoneResidencial:string;
+    snPossuiTelefoneResidencial:boolean;
+    telefoneCelular:string;
+    mae:string;
+    pai:string;
+    snPaiDesconhecido:boolean;
+    tpDocumento:string;
+    nrDocumento:string;
+    orgaoExpedidorDocumento:string;
+    dataEmissaoDocumento:Date;
+    validadeDocumento:Date;
+    snValidadeDocumento:boolean;
+}
--- src/views/cadastro-sobre-voce/cadastro-sobre-voce.component.ts
+++ src/views/cadastro-sobre-voce/cadastro-sobre-voce.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from './../../models/user.model';
+import { Router } from '@angular/router';
+
+import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-sobre-voce',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroSobreVoceComponent implements OnInit {
 
-  constructor() { }
+  public user: UserModel;
 
-  ngOnInit() {
+  loading = false;
+  error = false;
+  imageurl = 'https://s3-us-west-2.amazonaws.com/dapesweb/dapes_verde.jpg';
+
+  constructor(private router: Router) { }
+
+  save(model: UserModel, isValid: boolean) {
+    // call API to save customer
+    this.loading = true;
+    console.log(model, isValid);
   }
 
+
+  ngOnInit() {
+    this.user = {
+      nome: '',
+      email: '',
+      cpf: '',
+      nacionalidade: '',
+      naturalidade: '',
+      sexo: '',
+      nascimento: new Date(),
+      estadoCivil: '',
+      conjuge: '',
+      telefoneResidencial: '',
+      snPossuiTelefoneResidencial: false,
+      telefoneCelular: '',
+      mae: '',
+      pai: '',
+      snPaiDesconhecido: false,
+      tpDocumento: '',
+      nrDocumento: '',
+      orgaoExpedidorDocumento: '',
+      dataEmissaoDocumento: new Date(),
+      validadeDocumento: new Date(),
+      snValidadeDocumento: false
+    };
+  }
 }
